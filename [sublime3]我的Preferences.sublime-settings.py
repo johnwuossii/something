46bfe66136c7f9d481a995dd2cc7d9e2bf192c1b
John <birdjohnwu@gmail.com>
2014-05-05 15:11:08 +0800

    Create [sublime3]我的Preferences.sublime-settings.py

---
+++ [sublime3]我的Preferences.sublime-settings.py
@@ -0,0 +1,58 @@
+// Settings in here override those in "Default/Preferences.sublime-settings",
+// and are overridden in turn by file type specific settings.
+{
+  "auto_complete": true,
+  "auto_indent": true,
+  "blod_folder_labels": true,
+  "brace_style": "collapse",
+  "break_chained_methods": false,
+  /*"color_scheme": "Packages/User/Flatland Monokai (SL).tmTheme",*/
+  "detect_indentation": true,
+  "draw_miniap_border": true,
+  "ensure_newline_at_eof_on_save": true,
+  "eval_code": false,
+  "font_face": "Inconsolata",
+  "font_options":
+  [
+    "directwrite"
+  ],
+  "font_size": 14,
+  "highlight_line": true,
+  "highlight_modified_tabs": true,
+  "ignored_packages":
+  [
+    "Vintage",
+    "SublimeCodeIntel"
+  ],
+    "indent_char": " ",
+    "indent_level": 0,
+  "indent_size": 1,
+  "indent_with_tabs": false,
+  "jslint_happy": true,
+  "keep_array_indentation": false,
+  "keep_function_indentation": false,
+  "line_padding_bottom": 1,
+  "line_padding_top": 1,
+  "max_preserve_newlines": 10,
+  "preserve_newlines": true,
+  "reveal-on-activate": true,
+  "rulers":
+  [
+  80
+  ],
+  "scroll_past_end": true,
+  "smart_indent": true,
+  /*"soda_classic_tabs": true,
+  "soda_folder_icons": true,*/
+  "space_before_conditional": true,
+  "space_before_line_starters": true,
+  "tab_size": 2,
+  "theme": "Soda Dark 3.sublime-theme",
+  "translate_tabs_to_spaces": true,
+  "trim_trailing_white_space_on_save": true,
+  "unescape_strings": false,
+  "use_tab_stops": true,
+  "vintage_start_in_command_mode": true,
+  "word_wrap": true,
+  "wrap_line_length": 0
+}
